b853c3ab01e30921355833ac0fcfc1f372ac7dab
Lilpringle21YT <josebalderas83747@gmail.com>
2026-07-25 19:59:52 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 import random
 
 # A simple dictionary to track user points: {user_id: points}
